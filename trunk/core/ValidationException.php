3625cc71f470d6264b0f07c428c24d8c904bd8b5
Chandler McWilliams <chandler@brysonian.com>
2006-05-21 01:48:37 +0000

    - added __toString and list for message output
    git-svn-id: http://svn.bytesofspring.com/saint@21 e3b8b7ff-6f0e-0410-aa8a-f924344b2456

--- trunk/core/ValidationException.php
+++ trunk/core/ValidationException.php
@@ -7,12 +7,14 @@ class ValidationException extends SaintException
 	public $code;
 	public $file;
 	public $line;
+	public $parent_class;
 	
 // ===========================================================
 // - Constructor
 // ===========================================================
-	function __construct($errors, $code=VALIDATION_ERROR, $file=false, $line=false) {
+	public function __construct($errors, $parent_class, $code=VALIDATION_ERROR, $file=false, $line=false) {
 		parent::__construct('A Validation Exception has occurred.', $code);
+		$this->parent_class = $parent_class;
 		if ($file !== false) $this->file = $file;
 		if ($line !== false) $this->line = $line;
 		$this->errors = $errors;
@@ -22,16 +24,25 @@ class ValidationException extends SaintException
 // ===========================================================
 // - MAKE THE ERROR MESSAGE FROM THE ERRORS ARRAY
 // ===========================================================
-	function get_message_from_errors() {
-		$msg = array();
+	public function get_message_from_errors() {
+		$msg = '<ul>';
 		foreach($this->errors as $err) {
 			if (strpos($err[2], ':property') === false) $err[2] = ":property ".$err[2];
 
-			$msg[] = ucfirst(trim(str_replace(':property', $err[0], $err[2])));
+			$msg .= '<li>'.ucfirst(trim(str_replace(':property', $err[0], $err[2]))).'</li>';
 		}
-		return join("<br />", $msg);
+		$msg .= '<ul>';
+		return $msg;
 	}
-
+	
+	public function __toString() {
+		$html = '<div class="error">';
+		$c = count($this->errors);
+		$html .= '<h2>'.$c.' error'.(($c>1)?'s were':' was').' encountered while trying to save this '.$this->parent_class.'.</h2>';
+		$html .= $this->get_message_from_errors();
+		$html .= '</div>';
+		return $html;
+	}
 
 }
 
