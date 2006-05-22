051c7e941b6c700c245d3f511ec4f811b0d85403
Chandler McWilliams <chandler@brysonian.com>
2006-05-22 04:10:46 +0000

    - date and time form helpers now return the current time if the value was invalid
    git-svn-id: http://svn.bytesofspring.com/saint@53 e3b8b7ff-6f0e-0410-aa8a-f924344b2456

--- trunk/core/PHTMLView.php
+++ trunk/core/PHTMLView.php
@@ -133,6 +133,7 @@ function select($obj, $name, $prop, $collection, $key, $value, $options=array())
 
 function date_field($obj, $name, $prop) {
 	$v = $obj?(is_object($obj)?$obj->$prop:$obj[$prop]):'';
+	if ($v == -1 || $v === false) $v = Format::prettyDate();
 	$out = "<input type='text' name='{$name}[{$prop}]' value='$v' id='{$name}_$prop' size='40' maxlength='100' />\n";
 	$out .= "<span class='tip'><a href='#' onclick='d=new Date(); ds=d.toString(); ds=ds.split(\" \"); v=ds[1]+\" \"+ds[2]+\", \"+ds[3]; document.getElementById(\"{$name}_$prop\").value=v;return false;'>Insert Current Date</a></span>";
 	return $out;
@@ -140,6 +141,7 @@ function date_field($obj, $name, $prop) {
 
 function datetime_field($obj, $name, $prop) {
 	$v = $obj?(is_object($obj)?$obj->$prop:$obj[$prop]):'';
+	if ($v == -1 || $v === false) $v = Format::prettyDateTime();
 	$out = "<input type='text' name='{$name}[{$prop}]' value='$v' id='{$name}_$prop' size='40' maxlength='100' />\n";
 	$out .= "<span class='tip'><a href='#' onclick='d=new Date(); ds=d.toString(); ds=ds.split(\" \"); v=ds[1]+\" \"+ds[2]+\", \"+ds[3]+\" \"+ds[4].substring(0,ds[4].lastIndexOf(\":\"))+(d.getHours()>11?\" PM\":\" AM\"); document.getElementById(\"{$name}_$prop\").value=v;return false;'>Insert Current Date and Time</a></span>";
 	return $out;
@@ -147,6 +149,7 @@ function datetime_field($obj, $name, $prop) {
 
 function time_field($obj, $name, $prop) {
 	$v = $obj?(is_object($obj)?$obj->$prop:$obj[$prop]):'';
+	if ($v == -1 || $v === false) $v = Format::prettyTime();
 	$out = "<input type='text' name='{$name}[{$prop}]' value='$v' id='{$name}_$prop' size='40' maxlength='100' />\n";
 	$out .= "<span class='tip'><a href='#' onclick='d=new Date(); ds=d.toString(); ds=ds.split(\" \"); v=ds[4].substring(0,ds[4].lastIndexOf(\":\"))+(d.getHours()>11?\" PM\":\" AM\"); document.getElementById(\"{$name}_$prop\").value=v;return false;'>Insert Current Time</a></span>";
 	return $out;
