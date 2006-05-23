81a025ebe5b6cd8c711df6506b35f230d3d2cbfa
Chandler McWilliams <chandler@brysonian.com>
2006-05-23 22:36:48 +0000

    fixed a bug where a value set to empty string wouldn't update.
    git-svn-id: http://svn.bytesofspring.com/saint@75 e3b8b7ff-6f0e-0410-aa8a-f924344b2456

--- trunk/core/DBRecord.php
+++ trunk/core/DBRecord.php
@@ -27,14 +27,15 @@ class DBRecord implements Iterator, Serviceable
 	protected $current;
 	protected $fields;
 
-	protected $validate_presence_of			= array();
-	protected $validate_numericality_of	= array();
-	protected $validate_date_of					= array();
-	protected $validate_uniqueness_of		= array();
-	protected $validate_format_of				= array();
-	protected $validate_politeness_of		= array();
+	# protected $validate_presence_of			= array();
+	# protected $validate_numericality_of	= array();
+	# protected $validate_date_of					= array();
+	# protected $validate_uniqueness_of		= array();
+	# protected $validate_format_of				= array();
+	# protected $validate_politeness_of		= array();
 	
-	protected $errors = array();
+	#protected $errors = array();
+	protected $validator = array();
 
 // ===========================================================
 // - CONSTRUCTOR
@@ -188,9 +189,9 @@ class DBRecord implements Iterator, Serviceable
 		# validate the data
 		$this->validate_builtins();
 		$this->validate();
-		if (!empty($this->errors)) {
+		if ($this->validator->errors()) {
 			$db = debug_backtrace();
-			throw new ValidationException($this->errors, get_class($this), VALIDATION_ERROR, $db[1]['file'], $db[1]['line']);
+			throw new ValidationException($this->validator->errors()->errors, get_class($this), VALIDATION_ERROR, $db[1]['file'], $db[1]['line']);
 		}
 
 
@@ -233,17 +234,15 @@ class DBRecord implements Iterator, Serviceable
 		# validate the data
 		$this->validate_builtins();
 		$this->validate();
-		if (!empty($this->errors)) {
+		if ($this->validator->errors()) {
 			$db = debug_backtrace();
-			throw new ValidationException($this->errors, get_class($this), VALIDATION_ERROR, $db[1]['file'], $db[1]['line']);
+			throw new ValidationException($this->validator->errors()->errors, get_class($this), VALIDATION_ERROR, $db[1]['file'], $db[1]['line']);
 		}
-
 		$sql = "UPDATE `".$this->get_table()."` SET ";
 		$props = array();
 		foreach ($this->data as $k=>$v) {
-			if (!empty($v)) {
-				$props[] = "$k='".$this->escape_string($v)."'";
-			}
+			if ((strpos($k, '_uid') !== false) && empty($v)) continue;
+			$props[] = "$k='".$this->escape_string($v)."'";
 		}	
 		
 		$sql .= join(',',$props)." WHERE id=".$this->escape_string($this->get_id());		
@@ -272,203 +271,31 @@ class DBRecord implements Iterator, Serviceable
 	}
 
 // ===========================================================
-// - VALIDATION OPERATIONS
+// - MIXINS
 // ===========================================================
-	# the general form of the validation adding methods,
-	# shared by most of them
-	public function validates_x_of(&$type, $args, $code) {
-		if ($args === false) {
-			$type = array();
-			return;
-		}
-		
-		# add the error message if there isn't one
-		if (!array_key_exists('message', $args)) {
-			$args['message'] = get_error_message($code, '');
-		}
-		
-		# add each prop as a key to the array
-		foreach($args as $k => $v) {
-			if (is_numeric($k)) {
-				$type[$v] = $args['message'];
-			}
+	function __call($method, $args) {
+		# validation
+		if (strpos($method, 'validates_') !== false) {
+			if (!$this->validator) $this->validator = new DBRecordValidator($this);
+			call_user_func_array(array($this->validator, $method), $args);
 		}
 	}
-
-	public function validates_presence_of($args) {
-		if (!is_array($args) && $args !== false) $args = func_get_args();
-		$this->validates_x_of($this->validate_presence_of, $args, VALIDATION_EMPTY);
-	}
-
-	protected function test_presence_of($prop, $msg) {
-		if (array_key_exists($prop, $this->data) && !empty($this->data[$prop]))
-			return true;
-
-		$this->add_error($prop, VALIDATION_EMPTY, $msg);
-		return false;
-	}
-	
-
-	
-	public function validates_numericality_of($args) {
-		if (!is_array($args) && $args !== false) $args = func_get_args();
-		$this->validates_x_of($this->validate_numericality_of, $args, VALIDATION_NUMERIC);
-	}
-	
-	protected function test_numericality_of($prop, $msg) {
-		if (!array_key_exists($prop, $this->data)) return true;
-
-		if (is_numeric($this->data[$prop]))
-			return true;
-
-		$this->add_error($prop, VALIDATION_NUMERIC, $msg);
-		return false;
-	}
-	
-
-	
-
-	public function validates_date_of($args) {
-		if (!is_array($args) && $args !== false) $args = func_get_args();
-		$this->validates_x_of($this->validate_date_of, $args, VALIDATION_DATE);
-	}
-
-	protected function test_date_of($prop, $msg) {
-		if (!array_key_exists($prop, $this->data)) return true;
-		if (empty($this->data[$prop])) return true;
-		$d = strtotime($this->data[$prop]);
-		if ($d === false || $d == -1) {
-			$this->add_error($prop, VALIDATION_DATE, $msg);
-			return false;
-		}
-		$this->data[$prop] = Format::mysqlDateTime($this->data[$prop]);
-		return true;
-	}
-
-
-	
-	public function validates_uniqueness_of($args) {
-		if (!is_array($args) && $args !== false) $args = func_get_args();
-		$this->validates_x_of($this->validate_uniqueness_of, $args, VALIDATION_UNIQUE);
-	}
-
-	protected function test_uniqueness_of($prop, $msg) {
-		if (!array_key_exists($prop, $this->data)) return true;
-
-		$c = get_class($this);
-		$where = "$prop = '".$this->escape_string($this->data[$prop])."'";
-		if ($this->get_id()) $where .= " AND ".$this->get_table().".id <> ".$this->get_id();
-		
-		$r = self::find_where($where, $c);
-		if ($r->num_rows() > 0) {
-			$this->add_error($prop, VALIDATION_UNIQUE, $msg);
-			return false;
-		}
-		return true;
-	}
-
-
-
-	public function validates_email_of($prop, $message=false) {
-		$this->validates_format_of($prop, '/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/', get_error_message(VALIDATION_EMAIL, $prop));
-	}
-	
-	public function validates_url_of($prop, $message=false) {
-		$this->validates_format_of($prop, 
-			'/(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/xis',
-			get_error_message(VALIDATION_URL, $prop));
-	}
-	
-	public function validates_format_of($prop, $format, $message=false) {
-		if ($prop === false) {
-			$this->validate_format_of = array();
-		} else {
-			# add the error message if there isn't one
-			$message = $message?$message:get_error_message(VALIDATION_FORMAT, $prop);
-
-			$this->validate_format_of[$prop] = array($format, $message);
-		}
-	}
-
-	protected function test_format_of($prop, $args) {
-		if (!array_key_exists($prop, $this->data)) return true;
-		if (empty($this->data[$prop])) return true;
-		if (preg_match($args[0], $this->data[$prop]) == 0) {
-			$this->add_error($prop, VALIDATION_FORMAT, $args[1]);
-			return false;
-		}
-		return true;
-	}
-
-
-	public function validates_politeness_of($args) {
-		if (!is_array($args) && $args !== false) $args = func_get_args();
-		$this->validates_x_of($this->validate_politeness_of, $args, VALIDATION_CURSE);
-	}
-
-	protected function test_politeness_of($prop, $msg) {
-		if (!array_key_exists($prop, $this->data)) return true;
-		$badwords = array("pis","piss","breasts","bastard","bastard","b*stard","f*cking","phuck","kike","fuck","f*ck","fuc","fu*k","fuc*","f**k","f_ck","f__k","f@ck","fu??","fa??","f*??","f'cking","fuckin'","shit","sh|t","sh!t","sh*t","shit's","shitty","nigger","n*gger","n-gger","n_gger","darky","darkies","asshole","cunt","c*nt","c_nt","c/nt,","c-nt","pussy","p*ssy","fucker","slut","sl*t","dickhead","d*ck","f*cker","n*gg*r","clit","prick","faggot","f*gg*t","b*tch","wh*re","f@ggot","tw@t","goddamn","godamn","d@mn","gnikcuf","motherfucker","dickhead","blowjob","cocksucker","c*ck","c*sucker","c*cks*cker","asswipe","assmunch","fucking","fucked","feck","whatafucking","fags","fag","fag","fag","fags","fags","shitting","shits","chink","buttsex","shithole","bunghole","butthole","bullshit","bullshitter","bullshiter","bullshiters","assman","shit","bullshit","assfucker","tit","tits","cuntrag","bitch","bitchie","bitchy","whore","motherfucker","vagina");
-		foreach ($badwords as $word) {
-			if (stripos(strtolower($this->data[$prop]), $word) !== false) {
-				$this->add_error($prop, VALIDATION_CURSE, $msg);
-				return false;
-			}
-		}
-		return true;
-	}
-	
 
 	
 // ===========================================================
 // - EXECUTE THE VALIDATION
 // ===========================================================
 	public function errors() {
-		if (!empty($this->errors)) 
-			return new ValidationException($this->errors, get_class($this));
+		return $this->validator->errors();
 	}
 
 	protected function add_error($name, $code, $message) {
-		$this->errors[] = array($name, $code, $message);
+		return $this->validator->add_error($name, $code, $message);
 	}
 	
 	# does the actual validation
 	protected function validate_builtins() {
-		# check all the validation lists and make sure each one is ok
-		$noErr = true;
-		
-		# presence
-		foreach($this->validate_presence_of as $prop => $msg) {
-			$noErr = $this->test_presence_of($prop, $msg);
-		}
-		
-		# numericality
-		foreach($this->validate_numericality_of as $prop => $msg) {
-			$noErr = $this->test_numericality_of($prop, $msg);
-		}
-
-		# date
-		foreach($this->validate_date_of as $prop => $msg) {
-			$noErr = $this->test_date_of($prop, $msg);
-		}
-		
-		# uniqueness
-		foreach($this->validate_uniqueness_of as $prop => $msg) {
-			$noErr = $this->test_uniqueness_of($prop, $msg);
-		}
-		
-		# format
-		foreach($this->validate_format_of as $prop => $args) {
-			$noErr = $this->test_format_of($prop, $args);
-		}
-		
-		# curse words
-		foreach($this->validate_politeness_of as $prop => $args) {
-			$noErr = $this->test_politeness_of($prop, $args);
-		}
-		
-
-		return $noErr;
+		return $this->validator->validate($this->data);
 	}
 	
 	protected function validate() {
