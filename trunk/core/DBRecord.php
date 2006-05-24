e2d4fcd5f80bedf668854aafdc890d05f972cde5
Chandler McWilliams <chandler@brysonian.com>
2006-05-24 22:10:19 +0000

    makes sure there ARE args before fiddling with the line number.
    git-svn-id: http://svn.bytesofspring.com/saint@94 e3b8b7ff-6f0e-0410-aa8a-f924344b2456

--- trunk/core/DBRecord.php
+++ trunk/core/DBRecord.php
@@ -315,8 +315,10 @@ class DBRecord implements Iterator, Serviceable
 		$line = $db[$i]['line']-1;
 		
 		# if the args are on multiple lines, we need to account for that
-		foreach($db[$i]['args'] as $k => $v) {
-			$line -= substr_count($v, "\n");
+		if (array_key_exists('args', $db[$i]) && is_array($db[$i]['args'])) {
+			foreach($db[$i]['args'] as $k => $v) {
+				$line -= substr_count($v, "\n");
+			}
 		}
 		
 		$c = array();
