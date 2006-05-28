b8dc5da46106c4a56c1ae037b1b727abdd365aeb
Chandler McWilliams <chandler@brysonian.com>
2006-05-28 20:39:55 +0000

    code cleaning.
    git-svn-id: http://svn.bytesofspring.com/saint@124 e3b8b7ff-6f0e-0410-aa8a-f924344b2456

--- trunk/core/DBRecordValidator.php
+++ trunk/core/DBRecordValidator.php
@@ -47,157 +47,6 @@ class DBRecordValidator {
 
 
 // ===========================================================
-// - VALIDATION OPERATIONS
-// ===========================================================
-	# the general form of the validation adding methods,
-	# shared by most of them
-	public function validates_x_of(&$type, $args, $code) {
-		if ($args === false) {
-			$type = array();
-			return;
-		}
-		
-		# add the error message if there isn't one
-		if (!array_key_exists('message', $args)) {
-			$args['message'] = get_error_message($code, '');
-		}
-		
-		# add each prop as a key to the array
-		foreach($args as $k => $v) {
-			if (is_numeric($k)) {
-				$type[$v] = $args['message'];
-			}
-		}
-	}
-
-	public function validates_presence_of($args) {
-		if (!is_array($args) && $args !== false) $args = func_get_args();
-		$this->validates_x_of($this->validate_presence_of, $args, VALIDATION_EMPTY);
-	}
-
-	protected function test_presence_of($prop, $msg) {
-		if (array_key_exists($prop, $this->data) && !empty($this->data[$prop]))
-			return true;
-
-		$this->add_error($prop, VALIDATION_EMPTY, $msg);
-		return false;
-	}
-	
-
-	
-	public function validates_numericality_of($args) {
-		if (!is_array($args) && $args !== false) $args = func_get_args();
-		$this->validates_x_of($this->validate_numericality_of, $args, VALIDATION_NUMERIC);
-	}
-	
-	protected function test_numericality_of($prop, $msg) {
-		if (!array_key_exists($prop, $this->data)) return true;
-
-		if (is_numeric($this->data[$prop]))
-			return true;
-
-		$this->add_error($prop, VALIDATION_NUMERIC, $msg);
-		return false;
-	}
-	
-
-	
-
-	public function validates_date_of($args) {
-		if (!is_array($args) && $args !== false) $args = func_get_args();
-		$this->validates_x_of($this->validate_date_of, $args, VALIDATION_DATE);
-	}
-
-	protected function test_date_of($prop, $msg) {
-		if (!array_key_exists($prop, $this->data) || empty($this->data[$prop]) || ($this->data[$prop] == '0000-00-00')) {
-			$this->data[$prop] = NULL;
-			return true;
-		}
-		$d = strtotime($this->data[$prop]);
-		if ($d === false || $d == -1) {
-			$this->add_error($prop, VALIDATION_DATE, $msg);
-			return false;
-		}
-		$this->data[$prop] = Format::mysqlDateTime($this->data[$prop]);
-		return true;
-	}
-
-
-	
-	public function validates_uniqueness_of($args) {
-		if (!is_array($args) && $args !== false) $args = func_get_args();
-		$this->validates_x_of($this->validate_uniqueness_of, $args, VALIDATION_UNIQUE);
-	}
-
-	protected function test_uniqueness_of($prop, $msg) {
-		if (!array_key_exists($prop, $this->data)) return true;
-
-		
-		$where = "$prop = '".$this->parent->escape_string($this->data[$prop])."'";
-		if ($this->parent->get_id()) $where .= " AND ".$this->parent->get_table().".id <> ".$this->parent->get_id();
-		
-		$r = DBRecord::find_where($where, array(), $this->parent_class);
-		if ($r->num_rows() > 0) {
-			$this->add_error($prop, VALIDATION_UNIQUE, $msg);
-			return false;
-		}
-		return true;
-	}
-
-
-
-	public function validates_email_of($prop, $message=false) {
-		$this->validates_format_of($prop, '/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/', get_error_message(VALIDATION_EMAIL, $prop));
-	}
-	
-	public function validates_url_of($prop, $message=false) {
-		$this->validates_format_of($prop, 
-			'/(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/xis',
-			get_error_message(VALIDATION_URL, $prop));
-	}
-	
-	public function validates_format_of($prop, $format, $message=false) {
-		if ($prop === false) {
-			$this->validate_format_of = array();
-		} else {
-			# add the error message if there isn't one
-			$message = $message?$message:get_error_message(VALIDATION_FORMAT, $prop);
-
-			$this->validate_format_of[$prop] = array($format, $message);
-		}
-	}
-
-	protected function test_format_of($prop, $args) {
-		if (!array_key_exists($prop, $this->data)) return true;
-		if (empty($this->data[$prop])) return true;
-		if (preg_match($args[0], $this->data[$prop]) == 0) {
-			$this->add_error($prop, VALIDATION_FORMAT, $args[1]);
-			return false;
-		}
-		return true;
-	}
-
-
-	public function validates_politeness_of($args) {
-		if (!is_array($args) && $args !== false) $args = func_get_args();
-		$this->validates_x_of($this->validate_politeness_of, $args, VALIDATION_CURSE);
-	}
-
-	protected function test_politeness_of($prop, $msg) {
-		if (!array_key_exists($prop, $this->data)) return true;
-		$badwords = array("pis","piss","breasts","bastard","bastard","b*stard","f*cking","phuck","kike","fuck","f*ck","fuc","fu*k","fuc*","f**k","f_ck","f__k","f@ck","fu??","fa??","f*??","f'cking","fuckin'","shit","sh|t","sh!t","sh*t","shit's","shitty","nigger","n*gger","n-gger","n_gger","darky","darkies","asshole","cunt","c*nt","c_nt","c/nt,","c-nt","pussy","p*ssy","fucker","slut","sl*t","dickhead","d*ck","f*cker","n*gg*r","clit","prick","faggot","f*gg*t","b*tch","wh*re","f@ggot","tw@t","goddamn","godamn","d@mn","gnikcuf","motherfucker","dickhead","blowjob","cocksucker","c*ck","c*sucker","c*cks*cker","asswipe","assmunch","fucking","fucked","feck","whatafucking","fags","fag","fag","fag","fags","fags","shitting","shits","chink","buttsex","shithole","bunghole","butthole","bullshit","bullshitter","bullshiter","bullshiters","assman","shit","bullshit","assfucker","tit","tits","cuntrag","bitch","bitchie","bitchy","whore","motherfucker","vagina");
-		foreach ($badwords as $word) {
-			if (stripos(strtolower($this->data[$prop]), $word) !== false) {
-				$this->add_error($prop, VALIDATION_CURSE, $msg);
-				return false;
-			}
-		}
-		return true;
-	}
-	
-
-	
-// ===========================================================
 // - EXECUTE THE VALIDATION
 // ===========================================================
 	
@@ -242,6 +91,172 @@ class DBRecordValidator {
 	}
 
 
+// ===========================================================
+// - VALIDATION OPERATIONS
+// ===========================================================
+	# the general form of the validation adding methods,
+	# shared by most of them
+	public function validates_x_of(&$type, $args, $code) {
+		if ($args === false) {
+			$type = array();
+			return;
+		}
+		
+		# add the error message if there isn't one
+		if (!array_key_exists('message', $args)) {
+			$args['message'] = get_error_message($code, '');
+		}
+		
+		# add each prop as a key to the array
+		foreach($args as $k => $v) {
+			if (is_numeric($k)) {
+				$type[$v] = $args['message'];
+			}
+		}
+	}
+
+// ===========================================================
+// - PRESENCE
+// ===========================================================
+	public function validates_presence_of($args) {
+		if (!is_array($args) && $args !== false) $args = func_get_args();
+		$this->validates_x_of($this->validate_presence_of, $args, VALIDATION_EMPTY);
+	}
+
+	protected function test_presence_of($prop, $msg) {
+		if (array_key_exists($prop, $this->data) && !empty($this->data[$prop]))
+			return true;
+
+		$this->add_error($prop, VALIDATION_EMPTY, $msg);
+		return false;
+	}
+	
+
+
+// ===========================================================
+// - NUMERICAL
+// ===========================================================
+	public function validates_numericality_of($args) {
+		if (!is_array($args) && $args !== false) $args = func_get_args();
+		$this->validates_x_of($this->validate_numericality_of, $args, VALIDATION_NUMERIC);
+	}
+	
+	protected function test_numericality_of($prop, $msg) {
+		if (!array_key_exists($prop, $this->data)) return true;
+
+		if (is_numeric($this->data[$prop]))
+			return true;
+
+		$this->add_error($prop, VALIDATION_NUMERIC, $msg);
+		return false;
+	}
+	
+
+	
+// ===========================================================
+// - DATE
+// ===========================================================
+	public function validates_date_of($args) {
+		if (!is_array($args) && $args !== false) $args = func_get_args();
+		$this->validates_x_of($this->validate_date_of, $args, VALIDATION_DATE);
+	}
+
+	protected function test_date_of($prop, $msg) {
+		if (!array_key_exists($prop, $this->data) || empty($this->data[$prop]) || ($this->data[$prop] == '0000-00-00')) {
+			$this->data[$prop] = NULL;
+			return true;
+		}
+		$d = strtotime($this->data[$prop]);
+		if ($d === false || $d == -1) {
+			$this->add_error($prop, VALIDATION_DATE, $msg);
+			return false;
+		}
+		$this->data[$prop] = Format::mysqlDateTime($this->data[$prop]);
+		return true;
+	}
+
+
+
+// ===========================================================
+// - UNIQUE
+// ===========================================================
+	public function validates_uniqueness_of($args) {
+		if (!is_array($args) && $args !== false) $args = func_get_args();
+		$this->validates_x_of($this->validate_uniqueness_of, $args, VALIDATION_UNIQUE);
+	}
+
+	protected function test_uniqueness_of($prop, $msg) {
+		if (!array_key_exists($prop, $this->data)) return true;
+
+		
+		$where = "$prop = '".$this->parent->escape_string($this->data[$prop])."'";
+		if ($this->parent->get_id()) $where .= " AND ".$this->parent->get_table().".id <> ".$this->parent->get_id();
+		
+		$r = DBRecord::find_where($where, array(), $this->parent_class);
+		if ($r->num_rows() > 0) {
+			$this->add_error($prop, VALIDATION_UNIQUE, $msg);
+			return false;
+		}
+		return true;
+	}
+
+
+
+// ===========================================================
+// - FORMAT
+// ===========================================================
+	public function validates_email_of($prop, $message=false) {
+		$this->validates_format_of($prop, '/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/', get_error_message(VALIDATION_EMAIL, $prop));
+	}
+	
+	public function validates_url_of($prop, $message=false) {
+		$this->validates_format_of($prop, 
+			'/(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/xis',
+			get_error_message(VALIDATION_URL, $prop));
+	}
+	
+	public function validates_format_of($prop, $format, $message=false) {
+		if ($prop === false) {
+			$this->validate_format_of = array();
+		} else {
+			# add the error message if there isn't one
+			$message = $message?$message:get_error_message(VALIDATION_FORMAT, $prop);
+
+			$this->validate_format_of[$prop] = array($format, $message);
+		}
+	}
+
+	protected function test_format_of($prop, $args) {
+		if (!array_key_exists($prop, $this->data)) return true;
+		if (empty($this->data[$prop])) return true;
+		if (preg_match($args[0], $this->data[$prop]) == 0) {
+			$this->add_error($prop, VALIDATION_FORMAT, $args[1]);
+			return false;
+		}
+		return true;
+	}
+
+
+// ===========================================================
+// - POLITENESS
+// ===========================================================
+	public function validates_politeness_of($args) {
+		if (!is_array($args) && $args !== false) $args = func_get_args();
+		$this->validates_x_of($this->validate_politeness_of, $args, VALIDATION_CURSE);
+	}
+
+	protected function test_politeness_of($prop, $msg) {
+		if (!array_key_exists($prop, $this->data)) return true;
+		$badwords = array("pis","piss","breasts","bastard","bastard","b*stard","f*cking","phuck","kike","fuck","f*ck","fuc","fu*k","fuc*","f**k","f_ck","f__k","f@ck","fu??","fa??","f*??","f'cking","fuckin'","shit","sh|t","sh!t","sh*t","shit's","shitty","nigger","n*gger","n-gger","n_gger","darky","darkies","asshole","cunt","c*nt","c_nt","c/nt,","c-nt","pussy","p*ssy","fucker","slut","sl*t","dickhead","d*ck","f*cker","n*gg*r","clit","prick","faggot","f*gg*t","b*tch","wh*re","f@ggot","tw@t","goddamn","godamn","d@mn","gnikcuf","motherfucker","dickhead","blowjob","cocksucker","c*ck","c*sucker","c*cks*cker","asswipe","assmunch","fucking","fucked","feck","whatafucking","fags","fag","fag","fag","fags","fags","shitting","shits","chink","buttsex","shithole","bunghole","butthole","bullshit","bullshitter","bullshiter","bullshiters","assman","shit","bullshit","assfucker","tit","tits","cuntrag","bitch","bitchie","bitchy","whore","motherfucker","vagina");
+		foreach ($badwords as $word) {
+			if (stripos(strtolower($this->data[$prop]), $word) !== false) {
+				$this->add_error($prop, VALIDATION_CURSE, $msg);
+				return false;
+			}
+		}
+		return true;
+	}
+	
 }
 
 ?>
