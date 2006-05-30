7bffd3568c561cf44ca5ca407d1e38995ff0e19a
Chandler McWilliams <chandler@brysonian.com>
2006-05-30 05:08:25 +0000

    more button_to fun.
    git-svn-id: http://svn.bytesofspring.com/saint@139 e3b8b7ff-6f0e-0410-aa8a-f924344b2456

--- trunk/core/PHTMLView.php
+++ trunk/core/PHTMLView.php
@@ -216,13 +216,13 @@ function button_to($name, $args=false, $confirm=false, $options=array()) {
 	}
 	
 	ob_start();
-	echo "<input type='button' onclick='";
+	echo "<input type='button' onclick=\"";
 	if ($confirm && array_key_exists("onclick", $options)) {
-		echo ' if (confirm(\'Are You Sure?\')) {'.$options['onclick'].'} else {return false;}"';
+		echo 'if (confirm(\'Are You Sure?\')) {'.$options['onclick'].'} else {return false;}"';
 		unset($options['onclick']);			
 	} else {
-		echo ' if (confirm(\'Are You Sure?\')) ;"';
-		echo "document.location.href=\"".url_for($args)."\"' ";
+		echo 'if (confirm(\'Are You Sure?\')) ';
+		echo "document.location.href='".url_for($args)."';\" ";
 	}
 	foreach($options as $k => $v) {
 		echo " $k=\"$v\"";
