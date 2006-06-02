63fea580e516e0e5b20cdc92deb45adcffde5163
Chandler McWilliams <chandler@brysonian.com>
2006-06-02 06:56:05 +0000

    saint now sets the timezone to avoid notices.
    git-svn-id: http://svn.bytesofspring.com/saint@149 e3b8b7ff-6f0e-0410-aa8a-f924344b2456

--- trunk/saint.php
+++ trunk/saint.php
@@ -136,6 +136,14 @@
 	}
 
 
+// ===========================================================
+// - SET THE TIMEZONE
+// ===========================================================
+if(!defined('TIME_ZONE')) {
+	date_default_timezone_set(date_default_timezone_get());
+} else {
+	date_default_timezone_set(TIME_ZONE);
+}
 
 
 // ===========================================================
