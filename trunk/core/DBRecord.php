3b5b860041ad081e71c8b30b4e73c8c5bf9751f4
Chandler McWilliams <chandler@brysonian.com>
2006-06-11 00:40:25 +0000

    moved validation error codes
    git-svn-id: http://svn.bytesofspring.com/saint@230 e3b8b7ff-6f0e-0410-aa8a-f924344b2456

--- trunk/core/DBRecord.php
+++ trunk/core/DBRecord.php
@@ -1074,4 +1074,22 @@ class RecordNotFound extends SaintException {}
 class AmbiguousClass extends SaintException {}
 
 
+// ===========================================================
+// - ERROR CODES
+// ===========================================================
+define('DUPLICATE_ENTRY', 1062);
+
+// for validation
+define('VALIDATION_ERROR',			2000);
+define('VALIDATION_EMPTY',			2001);
+define('VALIDATION_NUMERIC',		2002);
+define('VALIDATION_DATE',				2003);
+define('VALIDATION_UNIQUE',			2004);
+define('VALIDATION_FORMAT',			2005);
+define('VALIDATION_EMAIL',			2006);
+define('VALIDATION_CURSE',			2007);
+define('VALIDATION_URL',				2008);
+
+
+
 ?>
