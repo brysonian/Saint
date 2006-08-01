211b0b7267a2e98d61564ecc1c057003eaf581ec
Chandler McWilliams <chandler@brysonian.com>
2006-08-01 04:57:12 +0000

    added checkbox.
    git-svn-id: http://svn.bytesofspring.com/saint@339 e3b8b7ff-6f0e-0410-aa8a-f924344b2456

--- branches/DBRecord2/core/PHTMLView.php
+++ branches/DBRecord2/core/PHTMLView.php
@@ -239,6 +239,16 @@ function time_field($obj, $name, $prop) {
 	return $out;
 }
 
+function checkbox($obj, $name, $prop, $value=1) {
+	$v = $obj?(is_object($obj)?$obj->$prop:$obj[$prop]):'';
+	$out = "<input type='checkbox' value='$value' id='{$name}_{$prop}_box'";
+	if ($value == $v)	$out .= " checked='checked' ";
+	$out .= " onclick='document.getElementById(\"{$name}_$prop\").value = this.checked?this.value:0;' /><input type='hidden' id='{$name}_$prop' name='{$name}[{$prop}]' value='$value' />\n";
+	return $out;
+}
+
+
+
 function submit_to($name, $confirm=false) {
 	ob_start();
 	echo "<input type='submit'";
