db48e3534a752f41d5360e6cc543e5d252dcabe0
Chandler McWilliams <chandler@brysonian.com>
2008-07-03 00:48:08 +0000

    time for a new direction
    git-svn-id: http://svn.bytesofspring.com/saint@384 e3b8b7ff-6f0e-0410-aa8a-f924344b2456

---
+++ trunk/locale/en/error_messages.php
@@ -0,0 +1,44 @@
+<?php
+
+// holds all the messages for the various error codes. Can be localized in the future.
+$SAINT_ERROR_MESSAGES = array();
+
+
+
+// ===========================================================
+// - GENERAL
+// ===========================================================
+
+
+// ===========================================================
+// - DBRECORD
+// ===========================================================
+$SAINT_ERROR_MESSAGES[VALIDATION_EMPTY]		= ':property cannot be empty.';
+$SAINT_ERROR_MESSAGES[VALIDATION_NUMERIC] = ':property is not a number.';
+$SAINT_ERROR_MESSAGES[VALIDATION_DATE]		= ':property cannot be understood as a date.';
+$SAINT_ERROR_MESSAGES[VALIDATION_UNIQUE]	= ':property must be unique.';
+$SAINT_ERROR_MESSAGES[VALIDATION_FORMAT]	= ':property is invalid.';
+$SAINT_ERROR_MESSAGES[VALIDATION_EMAIL]		= ':property must be a valid email address.';
+$SAINT_ERROR_MESSAGES[VALIDATION_URL]			= ':property must be a a valid web address.';
+$SAINT_ERROR_MESSAGES[VALIDATION_CURSE]		= ':property cannot contain curse words.';
+
+
+
+// ===========================================================
+// - ERROR MESSAGES
+// ===========================================================
+function get_error_message($code, $property='') {
+	global $SAINT_ERROR_MESSAGES;
+	$property = str_replace('_', ' ', $property);
+	
+	if (array_key_exists($code, $SAINT_ERROR_MESSAGES)) {
+		$msg = $SAINT_ERROR_MESSAGES[$code];
+	} else {
+		$msg = $code;
+	}
+	return $msg;
+}
+
+
+
+?>
